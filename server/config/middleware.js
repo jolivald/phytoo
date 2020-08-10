0b7b1b91842b3a39db2d493219ac18ffb5b08ba5
JJonathan <jolivald.jonathan@gmail.com>
2020-08-10 19:08:20 +0200

    start middleware config

---
+++ server/config/middleware.js
@@ -0,0 +1,34 @@
+const { name, version } = require('../package.json');
+const { capitalize } = require('lodash');
+
+module.exports = {
+  settings: {
+
+    // core
+    public: {
+      defaultIndex: true
+    },
+    gzip: {
+      enabled: true    
+    },
+    responseTime: {
+      enabled: false
+    } ,
+    poweredBy: {
+      enabled: true,
+      value: `${capitalize(name)} - ${version}`
+    },
+
+    // security
+    csp: {
+      enabled: true
+    },
+    hsts: {
+      enabled: false // TODO: SSL?
+    },
+    xframe: {
+      enable: true,
+      value: 'DENY'
+    }
+  }
+};
