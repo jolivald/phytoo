a1d34ce39bca356fbedfd85be97abf715fad48a5
JJonathan <jolivald.jonathan@gmail.com>
2020-08-10 21:37:44 +0200

    start to create models

--- server/config/server.js
+++ server/config/server.js
@@ -2,6 +2,7 @@ module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
   admin: {
+    //url: '/vegeto',
     auth: {
       secret: env('ADMIN_JWT_SECRET', '6827174903d6ffa864d32623664af7ff'),
     },
