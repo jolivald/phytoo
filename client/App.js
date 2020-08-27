d98404fd8a05513e399c6f89e4d2547403cd0998
JJonathan <jolivald.jonathan@gmail.com>
2020-08-27 13:31:19 +0200

    start navbar

--- client/App.js
+++ client/App.js
@@ -1,10 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import Navbar from './components/Navbar';
 
 export default function App() {
   return (
     <View style={styles.container}>
+      <Navbar />
       <Text>Open up App.js to start working on your app!!!</Text>
       <StatusBar style="auto" />
     </View>
@@ -16,6 +18,6 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    //justifyContent: 'center',
   },
 });
