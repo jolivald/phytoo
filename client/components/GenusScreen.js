eea7adaa823e5923763b8245b9c1f67a56a759b8
JJonathan <jolivald.jonathan@gmail.com>
2020-10-03 09:49:56 +0200

    Improve genus screen

--- client/components/GenusScreen.js
+++ client/components/GenusScreen.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
-import { Text, ActivityIndicator } from 'react-native-paper';
+import { ActivityIndicator, List} from 'react-native-paper';
 import Markdown from 'react-native-markdown-renderer';
+import removeMD from 'remove-markdown';
 import { apiFetch } from '../utils';
 import ScreenWrapper from './ScreenWrapper';
 import ScreenTitle from './ScreenTitle';
@@ -13,12 +14,22 @@ const GenusScreen = props => {
       .then(response => response.json())
       .then(genus => {
         setGenusInfo(genus);
+        console.log('genus', genus);
       })
   }, []);
   return genusInfo
     ? (<ScreenWrapper {...props} style={{ marginBottom: 0 }}>
         <ScreenTitle label={genusInfo.name} />
         <Markdown>{genusInfo.description}</Markdown>
+        {genusInfo.plants.map(({ id, species, description }) => (
+          <List.Item
+            key={id}
+            title={genusInfo.name + ' ' + species.name}
+            description={removeMD(description)}
+            left={props => (<List.Icon {...props} icon="leaf" />)}
+            onPress={() => props.navigation.navigate('plant', { id })}
+          />
+        ))}
       </ScreenWrapper>)
     : (<ActivityIndicator
         animating={true}
--- server/api/genus/controllers/genus.js
+++ server/api/genus/controllers/genus.js
@@ -1,8 +1,19 @@
 'use strict';
+const { sanitizeEntity } = require('strapi-utils');
 
 /**
  * Read the documentation (https://strapi.io/documentation/v3.x/concepts/controllers.html#core-controllers)
  * to customize this controller
  */
 
-module.exports = {};
+module.exports = {
+  findOne: async ctx => {
+    console.log('genus findOne', typeof ctx);
+    const { id } = ctx.params;
+    const entity = await strapi.services.genus.findOne(
+      { id },
+      ['plants', 'plants.species']
+    );
+    return sanitizeEntity(entity, { model: strapi.models.vernacular });
+  }
+};
